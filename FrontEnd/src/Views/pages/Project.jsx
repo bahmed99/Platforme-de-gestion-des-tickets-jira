6c778448c21b1f9662118afd9c1c3d4725f45a0c
ahmed bahri <76647412+bahmed99@users.noreply.github.com>
2022-03-23 22:44:57 +0100

    project

--- FrontEnd/src/Views/pages/Project.jsx
+++ FrontEnd/src/Views/pages/Project.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-
+import React ,{useState} from "react";
+import { useParams } from "react-router-dom";
+import FilterBar from "../components/Filterbar.js";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 export default function Project() {
-    const { project } = useParams();
-    
+  const { project } = useParams();
+
+  const [data, setData] = useState([]);
+
   return (
-    <div><h1>{project}</h1></div>
-  )
+    <div className="container-scroller">
+      <div>
+        <Sidebar data={data} setData={setData} loading={false}/>
+        <div className="container-fluid page-body-wrapper">
+          <Navbar />
+        </div>
+        <div className="main-panel">
+          <div className="content-wrapper">
+          <FilterBar />
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  );
 }
