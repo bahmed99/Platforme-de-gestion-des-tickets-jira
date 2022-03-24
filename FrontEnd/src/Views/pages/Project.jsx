d881cc55c020c5aacf07e343879c3a730367b3b7
khadija03 <khadijamalek.makhlouf@ensi-uma.tn>
2022-03-24 22:14:18 +0100

    b

--- FrontEnd/src/Views/pages/Project.jsx
+++ FrontEnd/src/Views/pages/Project.jsx
@@ -1,7 +1,8 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import FilterBar from "../components/Filterbar.js";
 import Navbar from "../components/Navbar";
+import "../../Assets/styles/Home.scss";
 import Sidebar from "../components/Sidebar";
 export default function Project() {
   const { project } = useParams();
@@ -11,16 +12,16 @@ export default function Project() {
   return (
     <div className="container-scroller">
       <div>
-        <Sidebar data={data} setData={setData} loading={false}/>
+        <Sidebar data={data} setData={setData} loading={false} />
         <div className="container-fluid page-body-wrapper">
           <Navbar />
         </div>
         <div className="main-panel">
           <div className="content-wrapper">
-          <FilterBar />
+            <FilterBar />
           </div>
         </div>
-       
+
       </div>
     </div>
   );
