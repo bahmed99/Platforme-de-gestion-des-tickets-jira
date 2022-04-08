b99138c3623f51730c90c25bef2223b783cfab80
ahmed bahri <76647412+bahmed99@users.noreply.github.com>
2022-04-08 18:56:15 +0100

    charts

--- FrontEnd/src/components/Filterbar.js
+++ FrontEnd/src/components/Filterbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams} from "react-router-dom";
 
 import axios from "axios";
-// import "../Assets/css/Filterbar.styles.css";
+import "../Assets/css/Filterbar.styles.css";
 
 export default function Filterbar() {
   const { id } = useParams();
